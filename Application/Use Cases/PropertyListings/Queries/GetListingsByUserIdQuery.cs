aebfc9b5f29ba32b95cbb4d76e8278ee18450523
Cezara <aprofirei.cezara@gmail.com>
2025-01-05 19:53:48 +0200

    Added refactoring using Result pattern for ProperyListing and Transaction entities

--- Application/Use Cases/PropertyListings/Queries/GetListingsByUserIdQuery.cs
+++ Application/Use Cases/PropertyListings/Queries/GetListingsByUserIdQuery.cs
@@ -1,9 +1,10 @@
 ﻿using Application.DTOs;
+using Domain.Common;
 using MediatR;
 
 namespace Application.Use_Cases.PropertyListings.Queries
 {
-    public class GetListingsByUserIdQuery : IRequest<List<PropertyListingDto>>
+    public class GetListingsByUserIdQuery : IRequest<Result<List<PropertyListingDto>>>
     {
         public Guid UserId { get; set; }
     }
--- Application/utils/MappingProfile.cs
+++ Application/utils/MappingProfile.cs
@@ -5,6 +5,8 @@ using Domain.Entities;
 using Application.Use_Cases.ClientInquiries.Commands;
 using Application.Use_Cases.Users.CommandHandlers;
 using Application.Use_Cases.Users.Commands;
+using Application.Use_Cases.Transactions.Commands;
+using RealEstateManagement.Application.Transactions.Commands;
 
 namespace Application.utils
 {
@@ -21,6 +23,8 @@ namespace Application.utils
             CreateMap<UpdatePropertyListingCommand, PropertyListing>().ReverseMap();
             CreateMap<CreateClientInquiryCommand, ClientInquiry>().ReverseMap();
             CreateMap<UpdateClientInquiryCommand, ClientInquiry>().ReverseMap();
+            CreateMap<CreateTransactionCommand, Transaction>().ReverseMap();
+            CreateMap<UpdateTransactionCommand, Transaction>().ReverseMap();
             CreateMap<CreateUserCommand, User>().ReverseMap();
             CreateMap<UpdateUserCommand, User>().ReverseMap();
 
